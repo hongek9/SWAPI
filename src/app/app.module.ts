45ca51382207e971fcf9edd3ad716dd5525e7300
Cody <chunt46511@gmail.com>
2019-10-01 17:18:22 -0400

    Theres a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { DataDisplayComponent } from './data-display/data-display.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app.material.module'
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     DataDisplayComponent
-  ],
+  ],  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
