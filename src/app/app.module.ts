01b0cab02cdcebe9a9ff9914429cd39f4a4e217b
Cody <chunt46511@gmail.com>
2019-10-02 22:45:06 -0400

    code works, trying to style results into the center

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { DataDisplayComponent } from './data-display/data-display.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppMaterialModule } from './app.material.module'
+import { AppMaterialModule } from './app.material.module';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    DataDisplayComponent
-  ],  
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AppMaterialModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppMaterialModule,
+    HttpClientModule
   ],
+  entryComponents: [],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
+
+
+export interface Choice {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  searchResults: any;
 
-  constructor() { }
+  choices: Choice[] = [
+    {value: 'people', viewValue: 'People'},
+    {value: 'starships', viewValue: 'Ships'},
+    {value: 'films', viewValue: 'Films'}
+  ];
+
+  constructor(private searchService: SearchService) { }
 
   ngOnInit() {
   }
 
+  search(type: string, info: string): void {
+    this.searchService.getResults(type, info).subscribe(data => {
+      console.log(data);
+      this.searchResults = data;
+      console.log(this.searchResults.results);
+    });
+  }
 }
